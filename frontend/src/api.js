699cf8cc11c1246926e4e6be63da35ee2b5275af
Hamza Notfaji <hamza.noutfaji@gmail.com>
2025-11-07 03:56:16 +0200

    frontend deployment fixes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+const LOCAL_API = "http://127.0.0.1:8000";
+const PROD_API = "https://sawakart.onrender.com";
+
+let baseURL = LOCAL_API;
+
+// Test local API availability
+try {
+  const res = await fetch(`${LOCAL_API}/docs`);
+  if (!res.ok) baseURL = PROD_API;
+} catch {
+  baseURL = PROD_API;
+}
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000", 
+  baseURL,
+  withCredentials: true,
 });
 
 export default api;
