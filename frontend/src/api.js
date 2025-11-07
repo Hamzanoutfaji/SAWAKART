bc00fdc727e484fee386ce33b90ba5b3ab78e720
Hamza Notfaji <hamza.noutfaji@gmail.com>
2025-11-08 00:07:31 +0200

    fixed delay connection issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,16 +2,7 @@ import axios from "axios";
 
 const LOCAL_API = "http://127.0.0.1:8000";
 const PROD_API = "https://sawakart-docker.onrender.com";
-
-let baseURL = LOCAL_API;
-
-// Test local API availability
-try {
-  const res = await fetch(`${LOCAL_API}/docs`);
-  if (!res.ok) baseURL = PROD_API;
-} catch {
-  baseURL = PROD_API;
-}
+const baseURL = PROD_API;
 
 const api = axios.create({
   baseURL,
